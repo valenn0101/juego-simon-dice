372e456a2bfeceb0422ee417284c8155960f54ac
valenn0101 <veroncaceresvalentin@gmail.com>
2023-01-16 18:40:07 -0300

    Juego Terminado
    El usuario debe recordar muy bien que secuencia esta en juego!

--- js/main.js
+++ js/main.js
@@ -5,16 +5,60 @@ const botonRojo = document.getElementById("rojo");
 const botonVerde = document.getElementById("verde");
 const botonAzul = document.getElementById("azul");
 const botonAmarillo = document.getElementById("amarillo");
+const tablero = [botonRojo, botonVerde, botonAzul, botonAmarillo];
+let patronMaquina = [];
+let patronUsuario = [];
 
-const botonIniciar = document.getElementById("iniciar-juego")
+document.getElementById("iniciar-juego").addEventListener("click", comenzarJuego);
+botonRojo.addEventListener("click", presionarBoton);
+botonVerde.addEventListener("click", presionarBoton);
+botonAzul.addEventListener("click", presionarBoton);
+botonAmarillo.addEventListener("click", presionarBoton);
 
-botonIniciar.onclick = function comenzarJuego(){
-    subirNivel()
+
+function comenzarJuego() {
+  nuevoNivel();
 }
 
-function subirNivel(){
-    setTimeout(() =>{
-        nivel = nivel + 1;
-        $ronda.innerText = nivel
-    }, 1000)
+function nuevoNivel() {
+  $estado.innerText = "Turno de la maquina!";
+  setTimeout(() => {
+    nivel = nivel + 1;
+    $ronda.innerText = nivel;
+    let siguientePatron = Math.floor(Math.random() * 4);
+    let siguienteColor = tablero[siguientePatron];
+
+    señalarColor(siguienteColor);
+    patronMaquina.push(siguienteColor);
+    $estado.innerText = "Tu turno!";
+  }, 1000);
 }
+
+function señalarColor(tablero) {
+  tablero.classList.add("señalar");
+  setTimeout(() => {
+    tablero.classList.remove("señalar");
+  }, 400);
+}
+
+function presionarBoton(event) {
+    if($estado.innerText === "Tu turno!"){
+        let boton = event.target;
+        señalarColor(boton);
+        patronUsuario.push(boton);
+        if (boton === patronMaquina[patronUsuario.length-1]) {
+            if (patronMaquina.length === patronUsuario.length) {
+                nuevoNivel();
+                patronUsuario = [];
+            }
+        }else{
+            $estado.innerText = "Perdiste!"
+            $estado.classList.remove("alert-primary");
+            $estado.classList.add("alert-danger");
+            setTimeout(() => {
+                location.reload();
+            }, 1000);
+        }
+    }
+}
+
