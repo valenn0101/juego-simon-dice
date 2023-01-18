a025335a4785f7f5ef4330cbfb533d628b731808
valenn0101 <veroncaceresvalentin@gmail.com>
2023-01-18 10:56:42 -0300

    Cambios a las señales cuando juega la maquina y  funciones a los botones de dificultad
    Ahora cuando la maquina juega se señala mas fácilmente que color fue seleccionado.
    
    Ademas para la dificultad facil se agrega un historial de la secuencia para recordar los colores, en modo dificil esto se elimina.
    Falta corregir que al pasar el mouse por los botones se despliegue info sobre la dificultad

--- js/main.js
+++ js/main.js
@@ -7,37 +7,56 @@ const botonAzul = document.getElementById("azul");
 const botonAmarillo = document.getElementById("amarillo");
 const tableroDeJuego = document.getElementById("tablero");
 const tablero = [botonRojo, botonVerde, botonAzul, botonAmarillo];
-const nivelDeJuego = document.getElementById("dificultad-juego");
-console.log(nivelDeJuego.value);
+const infoDificultad = document.querySelectorAll(".infoDificultad");
+const mensajeFlotante = document.getElementById("mensajeFlotante");
 let patronMaquina = [];
 let patronUsuario = [];
 
-document.getElementById("iniciar-juego").addEventListener("click", comenzarJuego);
+document
+  .getElementById("iniciar-juego")
+  .addEventListener("click", comenzarJuego);
 botonRojo.addEventListener("click", presionarBoton);
 botonVerde.addEventListener("click", presionarBoton);
 botonAzul.addEventListener("click", presionarBoton);
 botonAmarillo.addEventListener("click", presionarBoton);
 
+infoDificultad.onmouseover = function () {
+  mensajeFlotante.style.display = "block";
+};
+infoDificultad.onmouseout = function () {
+  mensajeFlotante.style.display = "none";
+};
 
 function comenzarJuego() {
   nuevoNivel();
+  document.getElementById("iniciar-juego").disabled = true;
 }
 
 function nuevoNivel() {
   $estado.innerText = "Turno de la maquina!";
-  tableroDeJuego.classList.add("turno-maquina");
+  botonRojo.classList.add("turno-maquina");
+  botonVerde.classList.add("turno-maquina");
+  botonAzul.classList.add("turno-maquina");
+  botonAmarillo.classList.add("turno-maquina");
+
   setTimeout(() => {
+    if(nivelDeJuego == 1){
+      imprimirPatronMaquina(patronMaquina);
+    }
     nivel = nivel + 1;
     $ronda.innerText = nivel;
     let siguientePatron = Math.floor(Math.random() * 4);
     let siguienteColor = tablero[siguientePatron];
-
     señalarColor(siguienteColor);
+    siguienteColor.classList.remove("turno-maquina");
     patronMaquina.push(siguienteColor);
     $estado.innerText = "Tu turno!";
-   setTimeout(()=>{
-    tableroDeJuego.classList.remove("turno-maquina");
-   },500); 
+    setTimeout(() => {
+      botonRojo.classList.remove("turno-maquina");
+      botonVerde.classList.remove("turno-maquina");
+      botonAzul.classList.remove("turno-maquina");
+      botonAmarillo.classList.remove("turno-maquina");
+    }, 500);
   }, 1000);
 }
 
@@ -49,23 +68,33 @@ function señalarColor(tablero) {
 }
 
 function presionarBoton(event) {
-    if($estado.innerText === "Tu turno!"){
-        let boton = event.target;
-        señalarColor(boton);
-        patronUsuario.push(boton);
-        if (boton === patronMaquina[patronUsuario.length-1]) {
-            if (patronMaquina.length === patronUsuario.length) {
-                nuevoNivel();
-                patronUsuario = [];
-            }
-        }else{
-            $estado.innerText = "Perdiste!"
-            $estado.classList.remove("alert-primary");
-            $estado.classList.add("alert-danger");
-            setTimeout(() => {
-                location.reload();
-            }, 1000);
-        }
+  if ($estado.innerText === "Tu turno!") {
+    let boton = event.target;
+    señalarColor(boton);
+    patronUsuario.push(boton);
+    if (boton === patronMaquina[patronUsuario.length - 1]) {
+      if (patronMaquina.length === patronUsuario.length) {
+        nuevoNivel();
+        patronUsuario = [];
+      }
+    } else {
+      $estado.innerText = "Perdiste!";
+      tableroDeJuego.classList.add("turno-maquina");
+      $estado.classList.remove("alert-primary");
+      $estado.classList.add("alert-danger");
+      setTimeout(() => {
+        location.reload();
+      }, 1600);
     }
+  }
 }
 
+function imprimirPatronMaquina(patronMaquina) {
+  const historialDeColores = document.getElementById("historial");
+  historialDeColores.innerText = "";
+  patronMaquina.forEach( color => {
+    const $patron = document.createElement("li");
+    $patron.innerText = color.value;
+    historialDeColores.appendChild($patron);
+  });
+}
